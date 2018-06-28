b1f207e47a0c8af7747c940636a0a88303e63309
adamszhu <adamszhu97@gmail.com>
2018-06-28 16:12:38 +0800

    change database connection parameters

--- backend/database_connect.py
+++ backend/database_connect.py
@@ -13,10 +13,11 @@ import psycopg2
 def db_connect():
     cur = None
     try:
-        conn = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='2251'", connect_timeout=30)
+        conn = psycopg2.connect("dbname=postgres user=postgres host=localhost password=123456", connect_timeout=30)
         conn.autocommit = True #Auto commit must be on if you want changes to be immediate for insert/update
         cur = conn.cursor()
-        cur.itersize = 10000
-    except:
+        #cur.itersize = 10000
+    except Exception,e:
+        print e
         print "[ERROR] couldn't connect to the database"
     return cur
